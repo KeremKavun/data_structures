109e2ad99c1d8ad5459ead5bab636413491632ed
KeremKavun <209keremkavun@gmail.com>
2025-09-19 21:14:04 +0300

    Improved allocation logic

--- include/stack.h
+++ include/stack.h
@@ -10,21 +10,23 @@
 
 struct Stack
 {
+    char* contents;
     size_t capacity;
     size_t top;
     size_t obj_size;
-    void** contents;
 };
 
-// init stack, returns either EXIT_SUCCESS OR EXIT_FAILURE
-int init_st(struct Stack* st, size_t _obj_size);
-// push an item, returns either EXIT_SUCCESS OR EXIT_FAILURE
-int push(struct Stack* st, const void* _new);
-// pop an item, returns either item or NULL, you should free item since ownership is yours
+// init stack, returns 0 if it succeeds, 1 otherwise
+int init_st(struct Stack* st, size_t obj_size);
+// push an item, returns 0 if it succeeds, 1 otherwise
+int push(struct Stack* st, const void* new);
+// pop an item, returns either item or NULL
 void* pop(struct Stack* st);
 // peek an item, returns either item or NULL
 void* peek_st(const struct Stack* st);
+// walk stack by given handler defined by the user according to item type they enqueue
+void walk_st(const struct Stack* st, void* userdata, void (*handler) (void* item, void* userdata));
 // free stack contents (freeing stack itself, if dynamically allocated, is on you)
-void free_st(struct Stack* st);
+void free_st(struct Stack* st, void* userdata, void (*deallocator) (void* item, void* userdata));
 
 #endif // STACK_H
--- src/stack.c
+++ src/stack.c
@@ -4,25 +4,27 @@
 #include <string.h>
 #include <errno.h>
 
-int init_st(struct Stack* st, size_t _obj_size)
+static size_t index(size_t ind, size_t obj_size);
+
+int init_st(struct Stack* st, size_t obj_size)
 {
-    st->capacity = 2;
-    st->top= 0;
-    st->obj_size = _obj_size;
-    st->contents = malloc(sizeof(void*) * st->capacity);
+    st->contents = malloc(sizeof(char) * obj_size * st->capacity);
     if (!st->contents)
     {
         LOG(LIB_LVL, CERROR, "Allocation failure");
         return 1;
     }
+    st->capacity = 2;
+    st->top= 0;
+    st->obj_size = obj_size;
     return 0;
 }
 
-int push(struct Stack* st, const void* _new)
+int push(struct Stack* st, const void* new)
 {
     if (st->capacity == size_st(st))
     {
-        void** new_contents = realloc(st->contents, sizeof(void*) * st->capacity * 2);
+        void** new_contents = realloc(st->contents, sizeof(char) * st->obj_size * st->capacity * 2);
         if (!new_contents)
         {
             LOG(LIB_LVL, CERROR, "Allocation failure");
@@ -31,13 +33,7 @@ int push(struct Stack* st, const void* _new)
         st->contents = new_contents;
         st->capacity *= 2;
     }
-    st->contents[st->top] = malloc(st->obj_size);
-    if (!st->contents[st->top])
-    {
-        LOG(LIB_LVL, CERROR, "Allocation failure");
-        return 1;
-    }
-    memcpy(st->contents[st->top], _new, st->obj_size);
+    memcpy(st->contents[index(st->top, st->obj_size)], new, st->obj_size);
     st->top++;
     return 0;
 }
@@ -47,7 +43,7 @@ void* pop(struct Stack* st)
     if (is_st_empty(st))
         return NULL;
     st->top--; 
-    void* item = st->contents[st->top];
+    void* item = st->contents[index(st->top, st->obj_size)];
     return item;
 }
 
@@ -55,16 +51,27 @@ void* peek_st(const struct Stack* st)
 {
     if (is_st_empty(st))
         return NULL;
-    return st->contents[st->top - 1];
+    return st->contents[index(st->top - 1, st->obj_size)];
 }
 
-void free_st(struct Stack* st)
+void walk_st(const struct Stack* st, void* userdata, void (*handler) (void* item, void* userdata))
 {
     for (size_t i = 0; i < size_st(st); i++)
-        free(st->contents[i]);
-    free(st->contents);
-    st->contents = NULL;
-    st->capacity = 2;
-    st->top= 0;
+        handler(st->contents[index(i, st->obj_size)], userdata);
 }
 
+void free_st(struct Stack* st, void* userdata, void (*deallocator) (void* item, void* userdata))
+{
+    if (deallocator)
+        walk_st(st, userdata, deallocator);
+    free(st->contents);
+    st->contents = NULL;
+    st->capacity = 2; st->top= 0;
+}
+
+// *** Helper functions *** //
+
+static inline size_t index(size_t ind, size_t obj_size)
+{
+    return ind * obj_size;
+}
