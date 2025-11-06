a649f5839e7d092a2de9b2bea117d1ded4149a39
KeremKavun <209keremkavun@gmail.com>
2025-11-07 00:59:47 +0300

    First commit of trees repo, implementing bst

---
+++ include/bst.h
@@ -0,0 +1,17 @@
+#ifndef BST_H
+#define BST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../../debug/include/debug.h"
+#include <stddef.h>
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BST_H
---
+++ src/trees.mk
@@ -0,0 +1,26 @@
+ifndef PROJECT_ROOT
+  $(error PROJECT_ROOT (Directory to put every C source files) is not defined. Please set it as an environment variable.)
+endif
+
+TREES_CC := gcc
+TREES_CFLAGS := -Wall -Wextra -O2 -MMD -MP
+
+TREES_SRC_DIR := $(PROJECT_ROOT)/trees/src
+TREES_BIN_DIR := $(PROJECT_ROOT)/trees/bin
+TREES_INCLUDE_DIR := $(PROJECT_ROOT)/trees/include
+
+TREES_SRC_FILES := $(wildcard $(TREES_SRC_DIR)/*.c)
+TREES_OBJ_FILES := $(patsubst $(TREES_SRC_DIR)/%.c,$(TREES_BIN_DIR)/%.o,$(TREES_SRC_FILES))
+TREES_DEP_FILES := $(TREES_OBJ_FILES:.o=.d)
+
+$(TREES_BIN_DIR)/libTREEs.a: $(TREES_OBJ_FILES)
+	ar rcs $@ $^ *.o
+
+$(TREES_BIN_DIR)/%.o: $(TREES_SRC_DIR)/%.c | $(TREES_BIN_DIR)
+	$(TREES_CC) $(TREES_CFLAGS) -I$(TREES_INCLUDE_DIR) -c $< -o $@
+
+# Create bin directory
+$(TREES_BIN_DIR):
+	@mkdir -p $@
+
+-include $(TREES_DEP_FILES)
