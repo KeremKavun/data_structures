704effb9a1dadc3b096251e463fb2ecedb382591
KeremKavun <209keremkavun@gmail.com>
2025-11-25 21:29:35 +0300

    Priority queue added (Just exact wrapper of heaps, i didnt like this, maybe revisit later)

---
+++ include/priority_queue.h
@@ -0,0 +1,47 @@
+#ifndef PRIORITY_QUEUE_H
+#define PRIORITY_QUEUE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../../debug/include/debug.h"
+#include <stddef.h>
+
+struct priority_queue;
+typedef struct priority_queue priority_queue_t;
+
+/*───────────────────────────────────────────────
+ * Lifecycle
+ *───────────────────────────────────────────────*/
+
+int priority_queue_init(struct priority_queue* pq, int (*cmp) (const void* a, const void* b));
+void priority_queue_deinit(struct priority_queue* pq, void* context, void (*deallocator) (void* item, void* context));
+
+/*───────────────────────────────────────────────
+ * Operations
+ *───────────────────────────────────────────────*/
+
+int priority_queue_enqueue(struct priority_queue* pq, void* item);
+void* priority_queue_dequeue(struct priority_queue* pq);
+
+/*───────────────────────────────────────────────
+ * Accessors
+ *───────────────────────────────────────────────*/
+
+const void* priority_queue_front(const struct priority_queue* pq);
+const void* priority_queue_rear(const struct priority_queue* pq);
+int priority_queue_empty(const struct priority_queue* pq);
+size_t priority_queue_size(const struct priority_queue* pq);
+
+/*───────────────────────────────────────────────
+ * Iterations
+ *───────────────────────────────────────────────*/
+
+void priority_queue_walk(struct priority_queue* pq, void* userdata, void (*handler) (void* item, void* userdata));
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // PRIORITY_QUEUE_H
---
+++ src/priority_queue.c
@@ -0,0 +1,75 @@
+#include "../include/priority_queue.h"
+#include "../../trees/src/heap.c"
+#include "../../trees/internals/heap_definition.h"
+#include <stdlib.h>
+
+struct priority_queue
+{
+    struct heap heap;
+};
+
+/*───────────────────────────────────────────────
+ * Lifecycle
+ *───────────────────────────────────────────────*/
+
+int priority_queue_init(struct priority_queue* pq, int (*cmp) (const void* a, const void* b))
+{
+    if (heap_init(&pq->heap, NULL, 0, 1, cmp) != 0)
+    {
+        LOG(LIB_LVL, CERROR, "Failed to create heap for priority queue");
+        return 1;
+    }
+    return 0;
+}
+
+void priority_queue_deinit(struct priority_queue* pq, void* context, void (*deallocator) (void* item, void* context))
+{
+    heap_deinit(&pq->heap, context, deallocator);
+}
+
+/*───────────────────────────────────────────────
+ * Operations
+ *───────────────────────────────────────────────*/
+
+int priority_queue_enqueue(struct priority_queue* pq, void* item)
+{
+    return heap_add(&pq->heap, item);
+}
+
+void* priority_queue_dequeue(struct priority_queue* pq)
+{
+    return heap_remove(&pq->heap);
+}
+
+/*───────────────────────────────────────────────
+ * Accessors
+ *───────────────────────────────────────────────*/
+
+const void* priority_queue_front(const struct priority_queue* pq)
+{
+    return *(void**)lbuffer_at((struct lbuffer*) &pq->heap.buffer, 0);
+}
+
+const void* priority_queue_rear(const struct priority_queue* pq)
+{
+    return *(void**)lbuffer_at((struct lbuffer*) &pq->heap.buffer, lbuffer_size((struct lbuffer*) &pq->heap.buffer) - 1);
+}
+
+int priority_queue_empty(const struct priority_queue* pq)
+{
+    return heap_empty(&pq->heap);
+}
+
+size_t priority_queue_size(const struct priority_queue* pq)
+{
+    return heap_size(&pq->heap);
+}
+
+/*───────────────────────────────────────────────
+ * Iterations
+ *───────────────────────────────────────────────*/
+
+void priority_queue_walk(struct priority_queue* pq, void* userdata, void (*handler) (void* data, void* userdata))
+{
+    heap_walk(&pq->heap, userdata, handler);
+}
